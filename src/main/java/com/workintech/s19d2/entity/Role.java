f5c9450fafae388965eac961ce745a1023e6a87a
Anıl Sevinç <contact@anilsevinc.net>
2026-06-06 11:34:16 +0300

    Görev 1: Entity, DTO, Repository katmanları ve veritabanı yapılandırması eklendi

---
+++ src/main/java/com/workintech/s19d2/entity/Role.java
@@ -0,0 +1,26 @@
+package com.workintech.s19d2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "role", schema = "bank")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
